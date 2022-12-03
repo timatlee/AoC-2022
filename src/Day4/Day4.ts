9ec5d636754fa402c65e00a265b96d5d05d9a96a
Tim AtLee <timatlee@gmail.com>
2022-12-03 12:47:15 -0700

    day 4 prep

---
+++ src/Day4/Day4.ts
@@ -0,0 +1,22 @@
+import { ReadDataFile } from "../ReadDataFile/ReadDataFile"
+
+export function part1(filename:String):number {
+    let datas = ReadDataFile(__dirname + '/' + filename)
+
+    return -1
+}
+
+export function part2(filename:string):number {
+    let datas = ReadDataFile(__dirname + '/' + filename)
+
+    return -1
+}
+
+
+
+
+console.log(part1("demo.txt"))
+console.log(part1("p1.txt"))
+
+console.log(part2("demo.txt"))
+console.log(part2("p1.txt"))
