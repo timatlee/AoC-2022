394649fb74c33f06aa27fa816bcec251ccb2c43a
Tim AtLee <timatlee@gmail.com>
2022-12-05 15:42:46 -0700

    Day 6 scaffold

---
+++ src/day6/Day6.ts
@@ -0,0 +1,20 @@
+import { ReadDataFile } from "../ReadDataFile/ReadDataFile"
+
+
+export function part1(filename: String): string {
+    let datas = ReadDataFile(__dirname + '/' + filename)
+
+    return ""
+}
+
+export function part2(filename: String): string {
+    let datas = ReadDataFile(__dirname + '/' + filename)
+
+    return ""
+}
+
+console.log(part1("demo.txt"))
+console.log(part1("p1.txt"))
+
+//console.log(part2("demo.txt"))
+//console.log(part2("p1.txt"))
