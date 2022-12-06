fbdb144aa10c2f00539796e2811f54dc19707a9d
Tim AtLee <timatlee@gmail.com>
2022-12-05 22:26:12 -0700

    Day 7 Scaffold

---
+++ src/Day7/Day7.ts
@@ -0,0 +1,20 @@
+import { initial } from "lodash"
+import { ReadDataFile } from "../ReadDataFile/ReadDataFile"
+
+export function part1(filename: String): number {
+    let datas = ReadDataFile(__dirname + '/' + filename)
+
+    return -1
+}
+
+export function part2(filename: String): number {
+    let datas = ReadDataFile(__dirname + '/' + filename)
+
+    return -1
+}
+
+console.log(part1("demo.txt"))
+console.log(part1("p1.txt"))
+
+//console.log(part2("demo.txt"))
+//console.log(part2("p1.txt"))
